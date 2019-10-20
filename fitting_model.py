47da27c6ea3ffa0300973ff54d4a68bc3f0ebd68
Weiran <huangweiran1998@gmail.com>
2019-10-20 10:59:38 +0800

    Optimized function arguments

--- fitting_model.py
+++ fitting_model.py
@@ -5,13 +5,12 @@ from fitting_shape import fitting_shape
 from rotations import e2r
 
 
-def fitting_model(pt2d, cr, single_value, mu, keypoints, w_id_initial, w_exp_initial):
+def fitting_model(pt2d, cr, single_value, keypoints, w_id_initial, w_exp_initial):
     """
 
     :param pt2d: 87 x 2 array. Landmarks of current photo/frame.
     :param cr: 34530 x 50 x 47 array. The data of 150 people after PCA.
     :param single_value: 50 x 1 array. Guess should be personal bias vector.
-    :param mu: 11510 x 3 array. The blendshape of mean face.
     :param keypoints: 1 x 87 array. The indices of 87 landmarks.
     :param w_id_initial: 47 x 1 array. Initial values for identity parameters.
     :param w_exp_initial: 50 x 1 array. Initial values for expression parameters.
@@ -37,19 +36,19 @@ def fitting_model(pt2d, cr, single_value, mu, keypoints, w_id_initial, w_exp_ini
         assert pt3d.shape == (3, 87)
 
         # 1. pose estimation
-        theta, t3d, f = fitting_pose(pt3d, pt2d.T)
-        rot = e2r(theta)
+        rot, t3d, f = fitting_pose(pt3d, pt2d.T)
+        # rot = e2r(theta)
 
-        # # 2. expression estimation
-        # id3d = np.tensordot(key_cr, w_id, axes=(1,0)).squeeze()  # 261 x 47
-        # w_exp = fitting_expression(id3d, pt2d, rot, t3d, f, w_exp).reshape(-1, 1)  # 47 x 1
-        #
+        # 2. expression estimation
+        id3d = np.tensordot(key_cr, w_id, axes=(1,0)).squeeze()  # 261 x 47
+        w_exp = fitting_expression(id3d, pt2d, rot, t3d, f, w_exp).reshape(-1, 1)  # 47 x 1
+
         # # 3. shape estimation
         # exp3d = np.tensordot(key_cr, w_exp, axes=(2, 0)).squeeze()  # 261 x 50
         # assert exp3d.shape == (261, 50)
         # w_id = fitting_shape(exp3d, pt2d, rot, t3d, f, single_value)
 
-        # print('[cur_res] w_id: ', w_id)
-        # print('[cur_res] w_exp: ', w_exp)
+        print('[cur_res] w_id: ', w_id)
+        print('[cur_res] w_exp: ', w_exp)
 
     return f, rot, t3d, w_id, w_exp
